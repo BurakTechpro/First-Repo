3804d59066b0768b566c71272251b0af5040a38c
burak <burrak.techproed@gmail.com>
2022-11-04 23:16:10 +0300

    second

---
+++ src/main/java/Dongu1.java
@@ -0,0 +1,11 @@
+public class Dongu1 {
+  //1'den 10'a kadar sayıları ekrana yazdıran programı yazınız.
+
+
+    public static void main(String[] args) {
+
+        for (int i=1;i<11;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
